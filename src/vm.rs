0d588d0f113c8542b8403e1c830c4ada64907f2e
DDAN <55860416+DDAN-17@users.noreply.github.com>
2025-10-13 17:03:00 -0500

    Added s, p, @, #, e, f, and w. Refactoring.

--- src/vm.rs
+++ src/vm.rs
@@ -1,24 +1,38 @@
 use super::Tape;
+use crate::display_stack;
+use anyhow::Context as _;
+use colored::Colorize;
+use crossterm::{
+    cursor,
+    terminal::{self, ClearType},
+};
 use log::error;
-use std::io;
+use std::io::{self, Write};
 
 pub struct Vm<'src> {
     ptr: usize,
     src: &'src str,
     data: Tape<u8>,
+    debug: bool,
+    context_stack: Vec<Context>,
+    stack: Vec<u8>,
 }
 
 #[derive(Debug)]
 pub enum Context {
-    Loop(usize),
+    Zero(usize),
+    While(usize),
 }
 
 impl<'src> Vm<'src> {
-    pub fn new(src: &'src str) -> Self {
+    pub fn new(src: &'src str, debug: bool) -> Self {
         Vm {
             ptr: 0,
             src,
             data: Tape::default(),
+            debug,
+            context_stack: Vec::new(),
+            stack: Vec::new(),
         }
     }
 
@@ -36,10 +50,42 @@ impl<'src> Vm<'src> {
         self.ptr = i;
     }
 
+    pub fn debug(&mut self, stdout: &str) -> anyhow::Result<()> {
+        crossterm::execute!(
+            io::stdout(),
+            terminal::Clear(ClearType::Purge),
+            terminal::Clear(ClearType::All),
+            cursor::MoveTo(0, 0)
+        )?;
+
+        print!("{stdout}");
+        if !stdout.ends_with("\n") {
+            println!("{}\n", "%".black().on_white());
+        } else {
+            println!();
+        }
+
+        println!("{}", self.src);
+        println!("{}^", " ".repeat(self.ptr - 1));
+
+        println!();
+
+        println!("{}", self.data);
+
+        println!("{}", display_stack(&self.stack));
+        println!();
+
+        Ok(())
+    }
+
     pub fn run(&mut self) -> anyhow::Result<()> {
-        let mut stack: Vec<Context> = Vec::new();
+        let mut stdout = String::new();
 
         while let Some(c) = self.next_char() {
+            if self.debug {
+                self.debug(&stdout)?;
+            }
+
             match c {
                 '0'..='9' => {
                     self.data.write(c.to_digit(10).unwrap() as u8);
@@ -49,18 +95,59 @@ impl<'src> Vm<'src> {
                 'c' => {
                     let mut buf = String::new();
                     io::stdin().read_line(&mut buf)?;
-                    self.data.write(buf.trim().parse::<u8>()?);
+                    self.data
+                        .write(buf.trim().parse::<u8>().context("bad number input!")?);
                 }
                 'i' => {
                     let mut buf = String::new();
                     io::stdin().read_line(&mut buf)?;
-                    self.data.write(buf.trim().parse::<char>()? as u8);
+                    self.data
+                        .write(buf.trim().parse::<char>().context("bad character input!")? as u8);
+                }
+                's' => {
+                    let mut buf = String::new();
+                    io::stdin().read_line(&mut buf)?;
+                    let trimmed = buf.trim();
+                    for c in trimmed.bytes() {
+                        self.data.write(c);
+                        self.data.right();
+                    }
+                    self.data.write(0);
+                    self.data.head -= trimmed.len();
+                }
+                'p' => {
+                    let mut i = 0;
+                    while self.data.read() != 0 {
+                        let print = format!("{}", self.data.read() as char);
+                        if self.debug {
+                            stdout += print.as_str();
+                        } else {
+                            print!("{print}");
+                        }
+
+                        i += 1;
+                        self.data.right();
+                    }
+                    self.data.head -= i;
+                    io::stdout().flush()?;
                 }
                 'n' => {
-                    print!("{}", self.data.read());
+                    let print = format!("{}", self.data.read());
+                    if self.debug {
+                        stdout += print.as_str();
+                    } else {
+                        print!("{print}");
+                    }
+                    io::stdout().flush()?;
                 }
                 'o' => {
-                    print!("{}", self.data.read() as char);
+                    let print = format!("{}", self.data.read() as char);
+                    if self.debug {
+                        stdout += print.as_str();
+                    } else {
+                        print!("{print}");
+                    }
+                    io::stdout().flush()?;
                 }
                 '+' => {
                     let left = self.data.read();
@@ -81,7 +168,11 @@ impl<'src> Vm<'src> {
                     self.data.right();
                     let right = self.data.read();
                     self.data.left();
-                    self.data.write(left * right);
+                    if let Some(v) = left.checked_mul(right) {
+                        self.data.write(v);
+                    } else {
+                        error!("Cannot multiply {left} * {right}!");
+                    }
                 }
                 '/' => {
                     let left = self.data.read();
@@ -91,17 +182,93 @@ impl<'src> Vm<'src> {
                     self.data.write(left / right);
                 }
                 '[' => {}
-                ']' => match stack.pop() {
+                ']' => match self.context_stack.pop() {
                     None => {}
                     Some(c) => match c {
-                        Context::Loop(ptr) => {
+                        Context::Zero(ptr) => {
                             if self.data.read() != 0 {
                                 self.seek_char(ptr);
-                                stack.push(c);
+                                self.context_stack.push(c);
+                            }
+                        }
+                        Context::While(ptr) => {
+                            if self.data.read() == 0 {
+                                self.seek_char(ptr);
+                                self.context_stack.push(c);
                             }
                         }
                     },
                 },
+                '@' => {
+                    self.stack.push(self.data.read());
+                }
+                '#' => {
+                    if let Some(v) = self.stack.pop() {
+                        self.data.write(v);
+                    }
+                }
+                'e' => {
+                    if self.current_char() != Some('[') {
+                        error!("'e' should have a ']' after! Ignoring.");
+                    } else {
+                        self.next_char();
+                    }
+
+                    if self.data.read() == 0 {
+                        let mut stack_size = 0;
+                        while let Some(c) = self.next_char() {
+                            if c == ']' && stack_size == 0 {
+                                break;
+                            } else if c == ']' {
+                                stack_size -= 1;
+                            } else if c == '[' {
+                                stack_size += 1;
+                            }
+                        }
+                    }
+                }
+                'f' => {
+                    if self.current_char() != Some('[') {
+                        error!("'f' should have a ']' after! Ignoring.");
+                    } else {
+                        self.next_char();
+                    }
+
+                    if self.data.read() != 0 {
+                        let mut stack_size = 0;
+                        while let Some(c) = self.next_char() {
+                            if c == ']' && stack_size == 0 {
+                                break;
+                            } else if c == ']' {
+                                stack_size -= 1;
+                            } else if c == '[' {
+                                stack_size += 1;
+                            }
+                        }
+                    }
+                }
+                'w' => {
+                    if self.current_char() != Some('[') {
+                        error!("'w' should have a ']' after! Ignoring.");
+                    } else {
+                        self.next_char();
+                    }
+
+                    if self.data.read() == 0 {
+                        self.context_stack.push(Context::While(self.ptr));
+                    } else {
+                        let mut stack_size = 0;
+                        while let Some(c) = self.next_char() {
+                            if c == ']' && stack_size == 0 {
+                                break;
+                            } else if c == ']' {
+                                stack_size -= 1;
+                            } else if c == '[' {
+                                stack_size += 1;
+                            }
+                        }
+                    }
+                }
                 'z' => {
                     if self.current_char() != Some('[') {
                         error!("'z' should have a ']' after! Ignoring.");
@@ -110,7 +277,7 @@ impl<'src> Vm<'src> {
                     }
 
                     if self.data.read() != 0 {
-                        stack.push(Context::Loop(self.ptr));
+                        self.context_stack.push(Context::Zero(self.ptr));
                     } else {
                         let mut stack_size = 0;
                         while let Some(c) = self.next_char() {
@@ -126,6 +293,13 @@ impl<'src> Vm<'src> {
                 }
                 _ => error!("Unknown character '{c}'! Skipping."),
             }
+
+            if self.debug {
+                io::stdin().read_line(&mut String::new())?;
+            }
+        }
+        if self.debug {
+            self.debug(&stdout)?;
         }
 
         Ok(())
